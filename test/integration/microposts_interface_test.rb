9553575027c5255ba1158d4344d249114242528a
taku <kosmo.waizu0804@gmail.com>
2017-08-30 15:54:07 +0900

    13.3の演習まで完了

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:michael)
   end
@@ -9,7 +8,9 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   test "micropost interface" do
     log_in_as(@user)
     get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
     assert_select "div.pagination"
+    assert_select 'input[type=file]'
     # 無効な送信
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: {micropost: {content: ""}}
@@ -17,12 +18,15 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # 有効な送信
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: {micropost: {content: content}}
+      post microposts_path, params: {micropost: {content: content, picture: picture}}
     end
+    assert assigns(:microposts).picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
+    assert_match "#{@user.microposts.count} micropost", response.body
     # 投稿を削除する
     assert_select 'a', text: 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first
