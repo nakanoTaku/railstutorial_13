bfb155883fac0239370086185f17cca074970c5d
taku <kosmo.waizu0804@gmail.com>
2017-08-30 16:37:14 +0900

    テストが通るように修正

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -22,7 +22,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_difference 'Micropost.count', 1 do
       post microposts_path, params: {micropost: {content: content, picture: picture}}
     end
-    assert assigns(:microposts).picture?
+    assert assigns(:micropost).picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
